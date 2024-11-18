395892da222bb2e8b2a9e08825a96ef6b627319b
Cecilia Terra <catreanden@gmail.com>
2024-11-18 18:11:41 +0000

    last commit

--- src/app.js
+++ src/app.js
@@ -6,18 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  const numbers = ("A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K");
-  const simbols = ("♦", " ♥", "♠", "♣");
-  const redSimbols = ("♦", " ♥");
+  const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  const simbols = ["♦", " ♥", "♠", "♣"];
+  const redSimbols = ["♦", " ♥"];
 
   const randomNumbers = Math.floor(Math.random() * numbers.length);
   const randomSimbols = Math.floor(Math.random() * simbols.length);
 
   const simbol1 = document.getElementById("simbol");
-  simbol1.innerText = simbols[randomSimbols];
   const number1 = document.getElementById("number");
-  number1.innerText = numbers[randomNumbers];
   const simboltwo = document.getElementById("simbol2");
+
+  simbol1.innerText = simbols[randomSimbols];
+  number1.innerText = numbers[randomNumbers];
   simboltwo.innerText = simbols[randomSimbols];
 
   if (redSimbols.includes(simbols[randomSimbols])) {
